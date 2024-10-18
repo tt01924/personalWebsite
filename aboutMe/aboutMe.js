3e041ad6f7965131a859ddc493c0d309b090bc2b
Todd Taylor <toddtaylor@eduroam-int-dhcp-97-182-231.ucl.ac.uk>
2024-10-18 16:16:35 +0100

    work experience scaffolding html

--- aboutMe/aboutMe.js
+++ aboutMe/aboutMe.js
@@ -3,3 +3,22 @@ document.addEventListener("DOMContentLoaded", function () {
         window.location.href = '../index/index.html';
     }
 });
+
+// JavaScript for carousel functionality
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+const slides = document.querySelector('.slides');
+
+prevButton.addEventListener('click', () => {
+    slides.scrollBy({
+        left: -slides.offsetWidth,
+        behavior: 'smooth'
+    });
+});
+
+nextButton.addEventListener('click', () => {
+    slides.scrollBy({
+        left: slides.offsetWidth,
+        behavior: 'smooth'
+    });
+});
--- index/index.js
+++ index/index.js
@@ -1,23 +1,3 @@
-// JavaScript for carousel functionality
-const prevButton = document.querySelector('.prev');
-const nextButton = document.querySelector('.next');
-const slides = document.querySelector('.slides');
-
-prevButton.addEventListener('click', () => {
-    slides.scrollBy({
-        left: -slides.offsetWidth,
-        behavior: 'smooth'
-    });
-});
-
-nextButton.addEventListener('click', () => {
-    slides.scrollBy({
-        left: slides.offsetWidth,
-        behavior: 'smooth'
-    });
-});
-
-
 // Loading screen javascript
 var loadingScreen = document.querySelector(".loadingScreen");
 
